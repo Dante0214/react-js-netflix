a51f2806329188a245839cf949dfdb3874797eac
dante0214 <rn_chic@naver.com>
2024-09-02 00:14:01 +0900

    feat: 모바일 레이아웃 변경

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
@@ -6,11 +6,25 @@ import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import SearchIcon from "@mui/icons-material/Search";
 import { Outlet, useNavigate } from "react-router-dom";
-import { TextField, useMediaQuery } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  TextField,
+  useMediaQuery,
+} from "@mui/material";
 
 function AppLayout() {
   const nav = useNavigate();
   const isMobile = useMediaQuery("(max-width:600px)");
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
 
   return (
     <>
@@ -24,7 +38,7 @@ function AppLayout() {
               style={{ marginRight: "auto", height: "50px", cursor: "pointer" }}
             />
             <Box
-              sx={{ ml: 4, flexGrow: 1, display: { xs: "flex", md: "flex" } }}
+              sx={{ ml: 4, flexGrow: 1, display: { xs: "none", md: "flex" } }}
             >
               <Button
                 onClick={() => nav("/")}
@@ -40,26 +54,64 @@ function AppLayout() {
               </Button>
             </Box>
 
-            <Box sx={{ flexGrow: 0 }}>
+            <Box
+              sx={{
+                flexGrow: 0,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+              }}
+            >
               <TextField
                 placeholder="Search"
                 size="small"
                 sx={{
+                  width: "50%",
                   mr: 1,
                   background: "gray",
                   borderRadius: "4px",
                 }}
               />
 
-              {!isMobile && (
-                <Button variant="outlined" startIcon={<SearchIcon />}>
-                  Search
-                </Button>
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <SearchIcon />
+              </Box>
+
+              {isMobile && (
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="end"
+                  onClick={handleDrawerToggle}
+                >
+                  <MenuIcon />
+                </IconButton>
               )}
             </Box>
           </Toolbar>
         </Container>
       </AppBar>
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={handleDrawerToggle}
+        sx={{ display: { xs: "block", md: "none" } }}
+      >
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+          <List>
+            <ListItem disablePadding>
+              <ListItemButton onClick={() => nav("/")}>
+                <ListItemText primary="Home" />
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton onClick={() => nav("/movies")}>
+                <ListItemText primary="Movies" />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
       <Outlet />
     </>
   );
