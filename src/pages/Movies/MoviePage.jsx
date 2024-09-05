2abda5e9b3c707ef7a597d3e0d663d9f13bc51cf
dante0214 <rn_chic@naver.com>
2024-09-05 10:25:22 +0900

    feat: 모바일 레이아웃 변경

--- src/pages/Movies/MoviePage.jsx
+++ src/pages/Movies/MoviePage.jsx
@@ -3,6 +3,7 @@ import "./MoviePage.style.css";
 import { useSearchMovieQuery } from "../../hooks/useSearchMovie";
 import { useSearchParams } from "react-router-dom";
 import {
+  Box,
   Button,
   Container,
   FormControl,
@@ -11,11 +12,14 @@ import {
   Pagination,
   Select,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import MovieCard from "../../common/MovieCard/MovieCard";
 import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
 
 const MoviePage = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+
   const [selectedGenre, setSelectedGenre] = useState("");
   const [sortOption, setSortOption] = useState("");
   const [query, setQuery] = useSearchParams();
@@ -73,104 +77,106 @@ const MoviePage = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item md={4} xs={12} justifyContent="flex-start">
-          <Button
-            fullWidth
-            size="large"
-            onClick={handleResetFilter}
-            sx={{
-              background: "white",
-              color: "black",
-              "&:hover": {
-                backgroundColor: "white",
-              },
-            }}
-          >
-            전체보기
-          </Button>
-          <FormControl sx={{ mt: 3 }} fullWidth variant="outlined">
-            <Select
-              displayEmpty
-              value={selectedGenre}
-              onChange={handleChange}
-              label="Genre"
-              sx={{
-                color: "black",
-                backgroundColor: "white",
-                "& .MuiSelect-icon": {
+        <Grid item md={4} xs={12}>
+          <Grid container justifyContent={isMobile ? "center" : "flex-start"}>
+            <Grid item>
+              <Button
+                fullWidth
+                size="large"
+                onClick={handleResetFilter}
+                sx={{
+                  background: "white",
                   color: "black",
-                },
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "white",
-                },
-                "& .MuiMenuItem-root": {
-                  color: "white",
-                  backgroundColor: "rgba(0, 0, 0, 0.7)",
                   "&:hover": {
-                    backgroundColor: "rgba(255, 255, 255, 0.2)",
+                    backgroundColor: "white",
                   },
-                },
-              }}
-            >
-              <MenuItem value="">장르 선택</MenuItem>
-              {genres.map((genre) => (
-                <MenuItem key={genre.id} value={genre.id}>
-                  {genre.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <FormControl sx={{ mt: 3 }} fullWidth variant="outlined">
-            <Select
-              displayEmpty
-              value={sortOption}
-              onChange={handleSortChange}
-              label="Sort By"
-              sx={{
-                color: "black",
-                backgroundColor: "white",
-                "& .MuiSelect-icon": {
-                  color: "black",
-                },
-                "& .MuiOutlinedInput-notchedOutline": {
-                  borderColor: "white",
-                },
-                "& .MuiMenuItem-root": {
-                  color: "white",
-                  backgroundColor: "rgba(0, 0, 0, 0.7)",
-                  "&:hover": {
-                    backgroundColor: "rgba(255, 255, 255, 0.2)",
-                  },
-                },
-              }}
-            >
-              <MenuItem value="">정렬 선택</MenuItem>
-              <MenuItem value="release_date_desc">최신순</MenuItem>
-              <MenuItem value="release_date_asc">오래된 순</MenuItem>
-              <MenuItem value="popularity_desc">인기도 내림차순</MenuItem>
+                }}
+              >
+                전체보기
+              </Button>
+              <FormControl sx={{ mt: 3 }} fullWidth variant="outlined">
+                <Select
+                  displayEmpty
+                  value={selectedGenre}
+                  onChange={handleChange}
+                  label="Genre"
+                  sx={{
+                    color: "black",
+                    backgroundColor: "white",
+                    "& .MuiSelect-icon": {
+                      color: "black",
+                    },
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white",
+                    },
+                    "& .MuiMenuItem-root": {
+                      color: "white",
+                      backgroundColor: "rgba(0, 0, 0, 0.7)",
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.2)",
+                      },
+                    },
+                  }}
+                >
+                  <MenuItem value="">장르 선택</MenuItem>
+                  {genres?.map((genre) => (
+                    <MenuItem key={genre.id} value={genre.id}>
+                      {genre.name}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+              <FormControl sx={{ mt: 3 }} fullWidth variant="outlined">
+                <Select
+                  displayEmpty
+                  value={sortOption}
+                  onChange={handleSortChange}
+                  label="Sort By"
+                  sx={{
+                    color: "black",
+                    backgroundColor: "white",
+                    "& .MuiSelect-icon": {
+                      color: "black",
+                    },
+                    "& .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white",
+                    },
+                    "& .MuiMenuItem-root": {
+                      color: "white",
+                      backgroundColor: "rgba(0, 0, 0, 0.7)",
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.2)",
+                      },
+                    },
+                  }}
+                >
+                  <MenuItem value="">정렬 선택</MenuItem>
+                  <MenuItem value="release_date_desc">최신순</MenuItem>
+                  <MenuItem value="release_date_asc">오래된 순</MenuItem>
+                  <MenuItem value="popularity_desc">인기도 내림차순</MenuItem>
 
-              <MenuItem value="popularity_asc">인기도 오름차순</MenuItem>
-            </Select>
-          </FormControl>
+                  <MenuItem value="popularity_asc">인기도 오름차순</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item md={8} xs={12}>
-          <Grid container spacing={2}>
+          <Grid
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="center"
+          >
             {filteredMovies && filteredMovies.length > 0 ? (
               filteredMovies.map((movie, idx) => (
-                <Grid item key={idx} xs={12} sm={6} md={4}>
+                <Grid item key={idx} sm={6} md={4}>
                   <MovieCard movie={movie} />
                 </Grid>
               ))
             ) : (
-              <Grid
-                container
-                justifyContent="center"
-                alignItems="center"
-                xs={12}
-              >
-                <Grid item>
-                  <Typography variant="h4">검색 결과가 없습니다.</Typography>
-                </Grid>
+              <Grid item>
+                <Typography variant="h4">검색 결과가 없습니다.</Typography>
               </Grid>
             )}
           </Grid>
